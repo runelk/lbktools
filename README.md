70e955ff8f6887c8444ddb0eff2cd30a9ed84a61
Rune Lain Knudsen <r.l.knudsen909@gmail.com>
2016-11-11 10:23:28 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# LBKtools
+
+A collection of tools for the
+[LBK](http://www.hf.uio.no/iln/tjenester/kunnskap/samlinger/bokmal/veiledningkorpus/)
+corpus.
---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name='lbktools',
+    version='0.0.1',
+    url='',
+    license='MIT',
+    author='Rune Lain Knudsen',
+    install_requires=[],
+    cmdclass={},
+    description='Tools for the LBK corpus.',
+    scripts=[]
+)
